55cb31a5145f1dd4f752fc2c7c02ca734f07dba2
Michi Lehr <mlehr@jimdo.com>
2012-03-15 15:32:50 +0100

    adds simple test for board

--- tests/board_test.py
+++ tests/board_test.py
@@ -1,8 +1,14 @@
 import unittest
-import zbar
 from mock import Mock
 from src import board
 
 
 class BoardTest(unittest.TestCase):
-    pass
+    def setUp(self):
+        self.board_id = 5
+        self.cards = None
+
+    def test_constructor(self):
+        test_board = board.Board(self.board_id, self.cards)
+        self.assertEqual(test_board.board_id, self.board_id)
+
