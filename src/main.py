9d11c5660b9964cb63346189c48f4bbdd025e168
Michi Lehr <mlehr@jimdo.com>
2012-03-15 15:10:09 +0100

    adds args for import and export path

--- src/main.py
+++ src/main.py
@@ -3,8 +3,18 @@ import scanner
 import mapper
 import board
 import os
+from sys import argv
 
-dir = directory.Directory(os, '/home/michi/Apps/klopfer/photos/', ['jpg', 'jpeg'])
+import_dir = argv[1]
+export_dir = argv[2]
+
+if not os.path.isdir(import_dir):
+    raise IOError('Klopfer says: import directory is not a directory')
+
+if not os.path.isdir(export_dir):
+    raise IOError('Klopfer says: export directory is not a directory')
+
+dir = directory.Directory(os, import_dir, ['jpg', 'jpeg'])
 imagefile = dir.get_oldest_file()
 scan = scanner.Scanner(imagefile.name)
 informations = scan.scan()
@@ -12,6 +22,6 @@ mapping = mapper.Mapper(informations)
 board_id = mapping.board_id
 cards = mapping.get_cards()
 current_board = board.Board(board_id, cards)
-current_board.export_json('/home/michi/Desktop/')
+current_board.export_json(export_dir)
 
 
