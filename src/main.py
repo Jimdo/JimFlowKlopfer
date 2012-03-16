c94f4bb97d5e05cbd4bdb452e023a12c0cce025d
Michi Lehr <mlehr@jimdo.com>
2012-03-16 09:33:32 +0100

    adds code comments

--- src/main.py
+++ src/main.py
@@ -14,13 +14,23 @@ if not os.path.isdir(import_dir):
 if not os.path.isdir(export_dir):
     raise IOError('Klopfer says: export directory is not a directory')
 
+# open dir and get oldest file with the given extension
 dir = directory.Directory(os, import_dir, ['jpg', 'jpeg'])
 imagefile = dir.get_oldest_file()
+
+# open image
 scan = scanner.Scanner(imagefile.name)
 informations = scan.scan()
+
+# remove old image
 os.remove(imagefile.name)
+
+# load board_id and cards
 mapping = mapper.Mapper(informations)
 board_id = mapping.board_id
 cards = mapping.get_cards()
+
+# create board
 current_board = board.Board(board_id, cards)
+# write baord to json
 current_board.export_json(export_dir)
